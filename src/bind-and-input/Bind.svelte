775b51d4d0fbde73334b20acd7f5eb28f6a06fbf
afshin <shahinof10009@gmail.com>
2022-12-30 13:49:05 +0330

    bind:this for getting reference of a html element

--- src/bind-and-input/Bind.svelte
+++ src/bind-and-input/Bind.svelte
@@ -1,6 +1,7 @@
 <script>
   import CheckBoxes from "./CheckBoxes.svelte";
   import RadioButtons from "./RadioButtons.svelte";
+  import Ref from "./Ref.svelte";
 
 
 </script>
@@ -12,3 +13,4 @@
 <!-- markup -->
 <CheckBoxes inputs={["slim", "cage", "room" , "zeeme"]} on:selected={(e) => console.log(e.detail)}></CheckBoxes>
 <RadioButtons inputs={["red", "blue", "green"]} on:selected={(e) => console.log(e.detail)}></RadioButtons>
+<Ref></Ref>
---
+++ src/bind-and-input/Ref.svelte
@@ -0,0 +1,36 @@
+<script>
+    let nameInpt ;
+    let emailInpt ;
+    let ageInpt ; 
+
+    function see () {
+        console.dir(nameInpt)
+        console.dir(emailInpt)
+        console.dir(ageInpt)
+    }
+</script>
+
+
+<style>
+    #cot {
+        border: 1px solid silver; 
+    }
+</style>
+
+<h2>for seeing the getting ref from actual html element</h2>
+<div id="cot">
+    <label >
+        Name:
+        <input type="text" bind:this={nameInpt}>
+    </label>
+    <label >
+        Email:
+        <input type="email" bind:this={emailInpt}>
+    </label>
+    <label >
+        Age:
+        <input type="number" min="1" max="110" bind:this={ageInpt}>
+    </label>
+    <button on:click={see}>See The Console</button>
+</div>
+
